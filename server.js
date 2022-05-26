db5efc0f575278829920517f517b3b14072a917f
Andrew Wokabi <drewzote@Andrews-MBP.frontierlocal.net>
2022-05-25 19:44:38 -0700

    Adding relocated breadcud

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 // DEPENDENCIES
-const express = require('express')
+const express = require("express");
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
 
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>BreadCrud</h1>");
+});
+
 // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
-// LISTEN
 app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
-})
-
+  console.log("nomming at port", PORT);
+});
